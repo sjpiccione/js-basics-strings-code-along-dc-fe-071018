5d6e7f66d5b5322a6748247e10b1057802587af3
Steven Piccione <sjpiccione@gmail.com>
2018-08-09 23:19:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const currentUser = 'Grace Hopper';
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = 'Welcome' + currentUser.chatAt(0) + '!';
+const shortGreeting = 'Welcome, ' + currentUser.chatAt(0) + '!';
 
 
 
