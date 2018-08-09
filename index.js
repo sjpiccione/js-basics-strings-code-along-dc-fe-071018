96c21c1173e58204adee13f89c36d3ad4b143e37
Steven Piccione <sjpiccione@gmail.com>
2018-08-09 23:08:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Write your code in this file!
 const currentUser = 'Grace Hopper';
-const welcomeMessage = 'Wecome to Flatbook,' + currentUser + '!';
-const welcomeMsg1 = 'Wecome to Flatbook,';
-const welcomeMsg2 = welcomeMsg1.toUpperCase;
-console.log("welcomeMsg2,":);
+const welcomeMessage = 'Wecome to Flatbook,' + currentUser + '!'
+
 
 
